bfd9958c867af740eb3befa1faa8f9ca79fb2565
carlos <carloscorrea137@gmail.com>
2015-04-25 17:51:37 -0700

    a funny bug

--- index.js
+++ index.js
@@ -25,33 +25,54 @@ if (navigator.getUserMedia) {
         draw(video,context,backcontext,cw,ch);
       });
     }, function(err) {
-        console.log("The following error occured: " + err.name);
-      }
-    );
-  } else {
-    console.log("getUserMedia not supported");
-  }
-
-  function $ready(cb) {
-    document.addEventListener('DOMContentLoaded', cb);
-  }
-
-  function draw(v,c,bc,cw,ch) {
-    if(v.paused || v.ended) return false;
-    // First, draw it into the backing canvas
-    bc.drawImage(v,0,0,cw,ch);
-    // Grab the pixel data from the backing canvas
-    var idata = bc.getImageData(0,0,cw,ch);
-    var data = idata.data;
-    var w = idata.width;
-    var limit = data.length
-    // Loop through the subpixels, convoluting each using an edge-detection matrix.
-    for(var i = 0; i < limit; i++) {
-      if( i%4 == 3 ) continue;
-      data[i] = 127 + 2*data[i] - data[i + 4] - data[i + w*4];
+      console.log("The following error occured: " + err.name);
     }
-    // Draw the pixels onto the visible canvas
-    c.putImageData(idata,0,0);
-    // Start over!
-    setTimeout(draw,20,v,c,bc,cw,ch);
+  );
+} else {
+  console.log("getUserMedia not supported");
+}
+
+function mapper(idata) {
+  var data = idata.data;
+  var w = idata.width;
+  var h = idata.height;
+  var limit = data.length;
+  for(var row = 0; row < h; row++) {
+    for(var col = 0; col < Math.floor(w/2); col++) {
+      var otherCol = w - col - 1;
+      var colIndex = (col + (row * w)) * 4;
+      var otherColIndex = (otherCol + (row * w)) * 4;
+      // swap!
+      var temp = data[colIndex];
+      data[colIndex] = data[otherColIndex];
+      data[otherColIndex] = temp;
+    }
   }
+  // Loop through the subpixels, convoluting each using an edge-detection matrix.
+  //for(var i = 0; i < limit; i++) {
+  //  if( i%4 == 3 ) continue;
+  //  data[i] = 127 + 2*data[i] - data[i + 4] - data[i + w*4];
+  //}
+  return idata;
+}
+
+// pick the most dissimilar
+
+function requestDraw(v,c,bc,cw,ch) {
+  window.requestAnimationFrame(function(time) {
+    draw(v,c,bc,cw,ch, time);
+  });
+}
+
+function draw(v,c,bc,cw,ch, time) {
+  if(v.paused || v.ended) return false;
+  // First, draw it into the backing canvas
+  bc.drawImage(v,0,0,cw,ch);
+  // Grab the pixel data from the backing canvas
+  var idata = bc.getImageData(0,0,cw,ch);
+  idata = mapper(idata);
+  // Draw the pixels onto the visible canvas
+  c.putImageData(idata,0,0);
+  // Start over!
+  //requestDraw(v,c,bc,cw,ch);
+}
